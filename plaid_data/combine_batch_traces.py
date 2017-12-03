de1ca36c98d688a272552f90301c4b769dfbf538
Neal Jackson <nealjack@umich.edu>
2017-12-02 19:02:49 -0800

    Add script to generate a labeled feature matrix
    matrix contains all traces in plaid_paper_batch_data/

---
+++ plaid_data/combine_batch_traces.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+import numpy as np
+import glob
+
+fnames = glob.glob("plaid_paper_batch_data/*npy")
+
+test = np.load(fnames[0])
+
+# num traces x current | voltage | house | label
+data = np.ndarray((len(fnames), test.shape[0]*test.shape[1]+2), dtype=float)
+
+class_map = []
+
+for fname, data_row in zip(sorted(fnames), data):
+    class_name = fname.split('/')[1].split('-')[0]
+    if class_name not in class_map:
+        class_map.append(class_name)
+    trace = np.load(fname)
+    data_row[:-2] = np.reshape(trace, 2000)
+    data_row[-2] = class_map.index(class_name)
+    data_row[-1] = int(fname.split('-')[-2].split('house')[1])
+
+# save big data array
+np.save("traces_bundle", data)
+np.save("traces_class_map", class_map)
