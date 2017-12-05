f5d0f036dcc52f7ba3f62cd0310267365d7b3799
Branden Ghena <brghena@umich.edu>
2017-12-05 13:53:19 -0800

    Weight the random selections by classes

--- paper_test/dense_single_layer.py
+++ paper_test/dense_single_layer.py
@@ -4,7 +4,17 @@ import tensorflow as tf
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+import sys
 
+# Config:
+n_hidden    = 30*11
+n_input     = 2*2*500 # current & voltage, 2 AC cycles @ 30 kHz
+n_classes   = 11
+learning_rate = 0.001
+display_step= 500
+batch_size  = 50
+
+# ensure that we always "randomly" run in a repeatable way
 RANDOM_SEED = 42
 tf.set_random_seed(RANDOM_SEED)
 np.random.seed(RANDOM_SEED)
@@ -14,18 +24,23 @@ data = np.load("../plaid_data/traces_bundle.npy")
 np.random.shuffle(data)
 Data = data[:, 0:-2]
 Labels = data[:,-1]
+if Labels.max()+1 != n_classes:
+    print("Error: Number of classes doesn't match labels input")
+    sys.exit()
 Device_Names = data[:,-2] #XXX to be used eventually for unseen testing
 
-# Config:
-n_hidden    = 300
-n_input     = 2*2*500#int(2.52E3/60)
-n_classes   = 11
-learning_rate = 0.001
-n_steps     = 500
-display_step= 100
-batch_size  = 50
-if Labels.max()+1 != n_classes:
-    print("Number of classes doesn't match labels input")
+# determine probabilities of selection for each trace
+#  - the probability of selecting each class should be equal
+#  - the probability of selecting any trace within a single class should be equal
+class_prob = 1.0/n_classes
+trace_prob = np.zeros(n_classes)
+Data_probabilities = np.zeros(len(Labels))
+for label in Labels:
+    trace_prob[int(label)] += 1
+for index, count in enumerate(trace_prob):
+    trace_prob[index] = class_prob/count
+for index in range(len(Data_probabilities)):
+    Data_probabilities[index] = trace_prob[int(Labels[index])]
 
 # convert Labels from integers to one-hot array
 OneHotLabels = np.eye(n_classes)[Labels.astype(np.int64)]
@@ -70,23 +85,25 @@ with tf.Session() as sess:
 
     sess.run(init)
 
-    #for step in range(1, n_steps+1):
+    # always test on everything
+    test_nums = range(len(Data))
 
+    # iterate forever training model
     step = 1
     while True:
         step += 1
 
-        batch_nums = np.random.choice(Data.shape[0], batch_size) #XXX: Need to add a weight matrix here based on classes
-        test_nums = np.random.choice(Data.shape[0], 1700) #XXX: Where did 2000 come from???
+        # select data to train on and test on for this iteration
+        batch_nums = np.random.choice(Data.shape[0], batch_size, p=Data_probabilities)
 
+        # run training
         sess.run(train_op, feed_dict = {X: Data[batch_nums], Y: OneHotLabels[batch_nums]})
+
+        # check accuracy every N iterations
         if step % display_step == 0 or step == 1:
             # calculate batch loss and accuracy
             loss, acc = sess.run([loss_op, accuracy], feed_dict={X: Data[test_nums], Y: OneHotLabels[test_nums]})
             print("Step " + str(step) + ", Minibatch Loss= " + \
                   "{:.4f}".format(loss) + ", Training Accuracy= " + \
                   "{:.3f}".format(acc))
-
-    print("Optimization Finished")
-
 
