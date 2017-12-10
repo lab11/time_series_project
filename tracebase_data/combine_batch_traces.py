82f4ebbffb9dc51abaac21c0b96defaa297fc972
Neal Jackson <nealjack@umich.edu>
2017-12-09 22:53:51 -0800

    Add script to combine all traces into one array
    Also pad arrays to same size to make training easier

---
+++ tracebase_data/combine_batch_traces.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python3
+
+import numpy as np
+import glob
+import os
+from zlib import crc32
+
+fnames = glob.glob("numpy_arrays/*.npy")
+
+test = np.load(fnames[0])
+
+trace_array = []
+class_map = []
+name_map = []
+labels = np.ndarray(len(fnames))
+dev_ids= np.ndarray(len(fnames))
+
+index = -1
+for fname in sorted(fnames):
+    # get unique device name from the filename
+    # note: device name is a uniquely identifying string for that device including
+    #   the dataset, house number, and model/make of the device
+    device_name = os.path.basename(fname).split('_')[-2]
+
+    # create map of device names we are including
+    if device_name not in name_map:
+        name_map.append(device_name)
+
+    # get device class from the filename
+    class_name = os.path.basename(fname).split('_')[0]
+    if class_name not in class_map:
+        class_map.append(class_name)
+
+    # get the data row we should be filling with data
+    index += 1
+    #data_row = data[index]
+
+    # record data for this trace
+    trace = np.load(fname)
+    trace_array.append(trace)
+
+    #data_row[:-2, 0] = trace[:, 0]
+    #data_row[:-2, 1] = trace[:, 1]
+    labels[index] = name_map.index(device_name)
+    dev_ids[index] = class_map.index(class_name)
+
+min_len = 60*60*24
+max_len = 0
+for trace in trace_array:
+    length = len(trace)
+    if length > max_len:
+        max_len = length
+    elif length < min_len:
+        min_len = length
+
+data = np.zeros((len(fnames), max_len, 3), dtype=float)
+for i, data_row in enumerate(data):
+    length = len(trace_array[i])
+    data_row[:length] = trace_array[i][:]
+
+# save big data array
+dirname = "./"
+np.save(dirname + "traces_bundle", data)
+np.save(dirname + "trace_classes", labels)
+np.save(dirname + "trace_dev_ids", dev_ids)
+np.save(dirname + "traces_class_map", class_map)
+np.save(dirname + "traces_name_map", name_map)
